f632b0c57bb19294a515a4424819c4881b3de5de
nhlm <nihylum@gmail.com>
2018-11-29 04:17:54 +0100

    introducing mutable/immutable interfaces and archetype traits

---
+++ src/Immutable.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+/**
+ * HyDIS Framework
+ * 
+ * This file is part of the HyDIS Framework. The license file can
+ * be found at the root directory of this repository.
+ * 
+ * (c) 2018 Matthias 'nhlm' Kaschubowski
+ */
+namespace Pulsar;
+
+use Pulsar\Universe\Abstracts\ObjectArchetypeInterface;
+
+interface Immutable extends ObjectArchetypeInterface {
+    /**
+     * marker interface
+     */
+}
